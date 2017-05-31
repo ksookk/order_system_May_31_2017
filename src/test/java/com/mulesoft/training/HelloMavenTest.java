8181710a19c0b78bdadce1f3f7ec9ecbc4285e37
ksookk <ksookk@gmail.com>
2017-05-31 12:42:06 -0500

    Added Test Case 2 to define Content-type

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -17,6 +18,14 @@ public class HelloMavenTest extends FunctionalTestCase {
     	System.out.println("\n\n\n**** Dynamic HTTP Port used in Junit Test case #1 **** --->" + dynamicPort.getNumber()+ "\n\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
+
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      System.out.println("\n\n\n**** Dynamic HTTP Port used in Junit Test case #2 **** --->" + dynamicPort.getNumber()+ "\n\n\n");
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+    }    
     
     @Override
     protected String getConfigFile() {
